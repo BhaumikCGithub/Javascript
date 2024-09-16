35c55a9bcdbfafa7c889a82a851ae91675ebfa18
Chauhan <bhaumikchauhan1512@gmail.com>
2024-09-16 23:46:22 +0530

    Add 03_conversionOberation.js file

---
+++ 03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33";
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1
+// let valueinBoolean = Boolean(isLoggedIn);
+
+// console.log(isLoggedIn);
+// console.log(valueinBoolean);
+
+// 1 => true
+// "" => false
+// "Bhaumik" => true
+
+let sommeNumber = 33
+let valueInString = String(sommeNumber);
+
+console.log(valueInString);
+console.log(typeof(valueInString));
