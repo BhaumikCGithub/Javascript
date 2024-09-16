83d4c2e5f2435b11faf8dbe2839766020fb07c80
Chauhan <bhaumikchauhan1512@gmail.com>
2024-09-16 23:23:19 +0530

    add 02_datatype

--- 01_variables.js
+++ 01_variables.js
@@ -1,1 +1,18 @@
-console.log("Bhaumik");
+const accountId = 12344
+let accountEmail = "bhaumik@gmail.com"
+var accountPassward = "Bhomu1512"
+accounytCity = "Gondal"
+
+// accountId = 234
+accountEmail = "mihir4gmail.com"
+accountPassward = "4247678"
+accounytCity = "Ahmedabad"
+let acccountState;
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassward,accounytCity,acccountState])
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+//alert(3 + 3) we are using node JS not browser
+
+let name = "Bhaumik"
+let age = 22
+let isLoggedIn = true
+
+// number
+// bigint
+// string => ""
+// boolean
+// null => standalone value
+// undefined
+/* symbol => component banavi tyare koi particular componet
+ni uniqueness batavva */
+
+//object
+
+console.log(typeof undefined);
+
