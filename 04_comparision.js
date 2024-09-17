a97af639e7fac5c442c4d2a15901bbf41674b16b
Chauhan <bhaumikchauhan1512@gmail.com>
2024-09-17 15:04:57 +0530

    Add 04_comparision.js file

---
+++ 04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==2);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+/*The reason is that equality check (==) and comparisions opperator(<,>,>=,<=)
+work differently.
+
+Comparision convert null to a number, treating it as 0 That's why (3) null >= 0 is true 
+and (1) null > 0 is false. */
+
+console.log(undefined == 0);
+console.log("3" === 3);
