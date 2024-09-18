d244299476522fc3ba6a66dfa2823a81db3890af
Chauhan <bhaumikchauhan1512@gmail.com>
2024-09-18 23:05:44 +0530

    Add 05_numsandmaths.js file

---
+++ 06_numsandmaths.js
@@ -0,0 +1,26 @@
+const score = 400
+const balance = new Number(100)
+
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) // 100.00
+
+const num = 123.896
+
+//console.log(num.toPrecision(4))
+
+const num1 = 100000000000
+//console.log(num1.toLocaleString('en-IN'))
+
+
+
+// ************** Maths **************** 
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.random());
+
+
