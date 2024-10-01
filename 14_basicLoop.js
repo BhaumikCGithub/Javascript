e02696f5531d1396aeea3232f632503d2f5eda9d
Chauhan <bhaumikchauhan1512@gmail.com>
2024-10-01 22:44:51 +0530

    Add 14_basicLoop.js file

---
+++ 14_basicLoop.js
@@ -0,0 +1,67 @@
+// for loop
+
+/*for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        console.log("You are at number 5");
+    }
+    console.log(element);
+}*/
+
+
+/*for (let i = 0; i < 10; i++) {
+    console.log(`Outer lopp value ${i}`)
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner loop ${j} Outer lopp value ${i}`)
+    }
+}*/
+
+/*let heroes = ['flash', 'spiderman', 'batman'];
+for (let index = 0; index < heroes.length; index++) {
+    const element = heroes[index];
+    console.log(element)
+}*/
+
+// breal and continue
+/*for(let i = 1; i <= 20; i++) {
+    if(i == 5)
+    {
+        console.log('You are at Number 5');
+        break;
+    }
+    console.log(`Value of i is ${i}`);
+}*/
+
+/*for(let i = 1; i <= 10; i++) {
+    if(i == 5)
+    {
+        continue;
+        console.log('You are at Number 5');
+    }
+    console.log(`Value of i is ${i}`);
+}*/
+
+// ******************** While Loop ***************************
+
+/*let number = 1
+while (number <= 10) {
+    console.log(`Value is ${number}`)
+    number = number + 2
+}*/
+
+/*let heroes = ['batman', 'spiderman', 'ironman', 'flash']
+let i = 0
+while (i < heroes.length) {
+    console.log(heroes[i]);   
+    i++
+}*/
+
+
+// *********************** Do While Loop *********************************
+
+/*let score = 1
+//let score = 11
+do {
+    console.log(`Score value is ${score}`);
+    score++
+} while (score <= 10 );*/
